2820ef23eac153fdaaf6bf2c6d029714b5117578
sueKK <125456919+sueKK@users.noreply.github.com>
2025-06-24 20:16:48 +0300

    customize navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import {useLocation} from 'react-router-dom'
 
 const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
   return (
     <div>
-      <Navbar />
+      {!isOwnerPath && <Navbar />}
     </div>
   )
 }
